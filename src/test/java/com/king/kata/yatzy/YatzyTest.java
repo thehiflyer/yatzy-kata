e0a1e61994667f1c5ac413ad4e22193bd71ed52a
Per Malmén <per.malmen@king.com>
2021-02-23 00:22:52 +0100

    Updated project to JUnit 5 and latest mockito

--- src/test/java/com/king/kata/yatzy/YatzyTest.java
+++ src/test/java/com/king/kata/yatzy/YatzyTest.java
@@ -1,21 +1,21 @@
 package com.king.kata.yatzy;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
 
 	private YatzyScorer yatzyScorer;
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeEach
+	public void setUp() {
 		yatzyScorer = new YatzyScorer();
 	}
 
 	@Test
-	public void chanceScoresCorrectly() throws Exception {
+	public void chanceScoresCorrectly() {
 		int score = yatzyScorer.calculateScore(Category.CHANCE, new YatzyRoll(5, 2, 1, 4, 6));
-		Assert.assertEquals(18, score);
+		assertEquals(18, score);
 	}
 }
